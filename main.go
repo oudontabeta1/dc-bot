df46d60f1223ed84d1fb9e434bce39d738c43386
ouden <144187901+oudentabetai@users.noreply.github.com>
2026-06-27 15:25:48 +0900

    change env

--- main.go
+++ main.go
@@ -20,18 +20,25 @@ var (
 func main() {
 	sessionManager := &discord.DiscordSessionManager{}
 	if err := storage.ConfigMgr.Load(); err != nil {
-		log.Fatalf("設定ファイルの読み込みに失敗: %v", err)
+		log.Fatalf("Failed to load config: %v", err)
 	}
+
 	dgs = sessionManager.InitializeSession(storage.Envs.DISCORD_BOT_TOKEN)
 	dgs.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuilds | discordgo.IntentsGuildMembers | discordgo.IntentsAll | discordgo.PermissionSendMessages
-	if err := dgs.Open(); err != nil {
-		log.Fatalf("Discordセッションのオープンに失敗: %v", err)
-	}
+
 	dgs.AddHandler(discord.OnMessageCreate)
 	dgs.AddHandler(discord.OnInteractionCreate)
+	dgs.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %s", s.State.User.Username)
+	})
+
+	if err := dgs.Open(); err != nil {
+		log.Fatalf("Failed to open Discord session: %v", err)
+	}
 	defer dgs.Close()
+
 	sendStartupVersionLog(dgs)
-	log.Println("ボットが起動しました。Ctrl+Cで終了します。")
+	log.Println("Launched")
 
 	//deleteAllGlobalCommands(dgs, os.Getenv("APPLICATION_ID"))
 	discord.SyncCommands(dgs, "", storage.Envs.APPLICATION_ID)
@@ -49,9 +56,9 @@ func sendStartupVersionLog(s *discordgo.Session) {
 		return
 	}
 
-	msg := "起動しました。リリースバージョン: " + version
+	msg := "Launched Bot Version is: " + version
 	if _, err := s.ChannelMessageSend(storage.Envs.LOG_CHANNEL_ID, msg); err != nil {
-		log.Printf("起動ログの送信に失敗: %v", err)
+		log.Printf("Failed To Send Launch Log: %v", err)
 	}
 }
 
@@ -59,8 +66,8 @@ func deleteAllGlobalCommands(s *discordgo.Session, appID string) {
 	_, err := s.ApplicationCommandBulkOverwrite(appID, "", []*discordgo.ApplicationCommand{})
 
 	if err != nil {
-		log.Printf("グローバルコマンドの削除に失敗しました: %v", err)
+		log.Printf("Failed To Remove Global Commands: %v", err)
 		return
 	}
-	log.Println("すべてのグローバルコマンドを削除しました。")
+	log.Println("Succesfully Removed Global Commands。")
 }
--- storage/config.go
+++ storage/config.go
@@ -16,25 +16,48 @@ type Env struct {
 	OWNER_ID           string
 	LOG_CHANNEL_ID     string
 	IGNORE_GUILD_ID    string
+	APP_ENV            string
 }
 
 var Envs = loadEnvs()
 
 var ConfigMgr = NewConfigManager("config.json")
 
+var (
+	token          string
+	application_id string
+)
+
 func loadEnvs() Env {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	env := os.Getenv("APP_ENV")
+	if env == "" || env == "dev" || env == "development" {
+		env = "development"
+		application_id = os.Getenv("DEV_APPLICATION_ID")
+		token = os.Getenv("DEV_DISCORD_BOT_TOKEN")
+		log.Printf("Using Dev Enviroment")
+
+	}
+	if env == "production" || env == "prod" {
+		env = "production"
+		application_id = os.Getenv("PROD_APPLICATION_ID")
+		token = os.Getenv("PROD_DISCORD_BOT_TOKEN")
+		log.Printf("Using Prod Enviroment")
+
+	}
+
 	return Env{
-		APPLICATION_ID:     os.Getenv("APPLICATION_ID"),
-		DISCORD_BOT_TOKEN:  os.Getenv("DISCORD_BOT_TOKEN"),
+		APPLICATION_ID:     application_id,
+		DISCORD_BOT_TOKEN:  token,
 		TEST_GUILD_ID:      os.Getenv("TEST_GUILD_ID"),
 		PANEL_API_TOKEN:    os.Getenv("PANEL_API_TOKEN"),
 		PANEL_CLIENT_TOKEN: os.Getenv("PANEL_CLIENT_TOKEN"),
 		OWNER_ID:           os.Getenv("OWNER_ID"),
 		LOG_CHANNEL_ID:     os.Getenv("LOG_CHANNEL_ID"),
 		IGNORE_GUILD_ID:    os.Getenv("IGNORE_GUILD_ID"),
+		APP_ENV:            os.Getenv("APP_ENV"),
 	}
 }
